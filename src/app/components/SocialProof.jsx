1cecf1eabb0173b53a7b773d667f36c80cee6db8
Tauhid Febryan <tauhidsamp@gmail.com>
2026-06-01 17:01:39 +0700

    feat: tambah testimoni/social proof

---
+++ src/app/components/SocialProof.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SocialProof = () => {
+    return (
+        <section className='mt-16 bg-secondary'>
+            <div className='text-center'>
+                <h2 className='text-primary font-bold'>Lebih dari 1,200+ Kamar Telah Disulap Menjadi Aesthetic</h2>
+            </div>
+        </section>
+    )
+}
+
+export default SocialProof
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import React from 'react'
 import Hero from './components/Hero'
 import ProductShowcase from './components/ProductShowcase'
+import SocialProof from './components/SocialProof'
 
 const Page = () => {
   return (
     <>
       <Hero></Hero>
       <ProductShowcase></ProductShowcase>
+      <SocialProof></SocialProof>
     </>
   )
 }
